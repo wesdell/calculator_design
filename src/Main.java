3bee2e8c735f93a6de5a2552f76e0051ecea9650
Wesdell <wesdell17@gmail.com>
2024-06-08 18:45:59 -0500

    Implement tests

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 public class Main {
   public static void main(String[] args) {
-    Calculator calculator = new Calculator(
-            new RealNumber("10"),
-            new RealNumber("01")
-    );
+    Calculator calculator = new Calculator();
+    NumberType num1 = new RealNumber("10");
+    NumberType num2 = new RealNumber("01");
 
-    System.out.println(calculator.sum());
-    System.out.println(calculator.subtract());
-    System.out.println(calculator.multiply());
-    System.out.println(calculator.divide());
+    System.out.println(calculator.sum(num1, num2));
+    System.out.println(calculator.subtract(num1, num2));
+    System.out.println(calculator.multiply(num1, num2));
+    System.out.println(calculator.divide(num1, num2));
   }
 }
